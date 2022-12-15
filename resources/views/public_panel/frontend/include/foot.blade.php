0a1f2411a91962ce42658b3322323bd791915a69
SUBHAN <lionsubhan123@gmail.com>
2022-12-15 11:28:43 +0500

    Public_Panel Created

---
+++ resources/views/public_panel/frontend/include/foot.blade.php
@@ -0,0 +1,7 @@
+
+        
+    <!-- Linked Js Files -->
+    <script src="public_assets/js/popper.min.js"></script>
+    <script src="public_assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
